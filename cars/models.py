039ff1e4bade86a161ec5f214ee402fda41c9ddc
Mohamad Alhalabi <mh19.my@gmail.com>
2018-03-19 18:29:31 +0200

    Initial commit

---
+++ cars/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class CarModel(models.Model):
+    ModelName = models.CharField(max_length=40)
+    CarBrand = models.CharField(max_length=45)
+    Year = models.CharField(max_length=40)
+    Engine = models.CharField(max_length=40)
+    Cylinders = models.CharField(max_length=16)
+    DoorsNum = models.CharField(max_length=40)
+    Weight = models.CharField(max_length=40)
+    Fuel = models.CharField(max_length=15)
+    BodyType = models.CharField(max_length=20)
+    Transmission = models.CharField(max_length=45)
+    HP = models.CharField(max_length=40)
+    TopSpeed = models.CharField(max_length=40)
+    FuelCapacity = models.CharField(max_length=40)
+    Country = models.CharField(max_length=45)
+    Mileage = models.CharField(max_length=40)
+    Color = models.CharField(max_length=45)
