211ab3a9139b0877e5f70f677860f20bca52a2c3
Mohamad Alhalabi <mh19.my@gmail.com>
2018-04-02 18:27:19 +0300

    Added basic /cars/ and /cars/id views templates

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import CarModel
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login
 from django.views.generic import View
